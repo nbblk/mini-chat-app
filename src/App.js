da8e4264315bf231f26f1a203da9e7f57f7c1148
blks <nbblks@gmail.com>
2020-12-14 22:00:45 +0900

    Add tests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    username: '',
+    isSubmitted: false,
+    message: ''
+  }
+
+  submitHandler(event) {
+    event.preventDefault();
+    if (this.state.username) {
+      localStorage.setItem('username', event.target.value);
+      this.setState({ isSubmitted: true });
+    }
+  }
+
+  inputChangeHandler(event) {
+    this.setState({ username: event.target.value });
+  }
+
+  messageHandler(event) {
+    this.setState({ message: event.target.value });
+  }
+
+  enterHandler(event) {
+    event.preventDefault();
+    // request to server
+  }
+
+  render () {
+    let element = null;
+    if (this.state.isSubmitted) {
+      element = <section data-testid="chat" className="chat">
+      <form aria-label="form" onSubmit={(event) => this.enterHandler(event)}>
+        <textarea></textarea>
+        <input type="text" data-testid="chat-text" placeholder="Enter a message" onChange={(event) => this.messageHandler()}/>
+        <button type="submit">Enter</button>
+      </form>
+      </section>
+    } else {
+      element = <section data-testid="landing" className="landing">
+      <h1>Welcome to chatty!</h1>
+      <form aria-label="form" onSubmit={(event) => this.submitHandler(event)}>
+      {!this.state.username ? <small role="alert" style={{'color': 'red'}}>Please enter a username</small> : null }
+      <input type="text" data-testid="landing-text" placeholder="Enter a username" onChange={(event) => this.inputChangeHandler(event)}/>
+      <button type="submit">Submit</button></form>
+      </section>
+    }
+    return <div className="App">{element}</div>
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders username and button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByTestId('landing-text')).toBeInTheDocument();
+  expect(screen.getByRole('button')).toBeInTheDocument();
+  expect(screen.getByTestId('landing')).toHaveClass('landing');
+});
+
+test('alert when input is empty', () => {
+  render(<App />);
+  userEvent.type('');
+  expect(screen.getByRole('alert')).toHaveTextContent('Please enter a username');
+  expect(screen.getByRole('alert')).toHaveStyle('color: red');
+});
+
+test('renders chat after clicking submit', async () => {
+  render(<App />);
+  userEvent.type(screen.getByRole('textbox'), 'username');
+  await userEvent.click(screen.getByText('Submit'));
+  expect(screen.getByTestId('chat')).toHaveClass('chat');
+  expect(screen.getByTestId('chat-text')).toHaveAttribute('placeholder', 'Enter a message');
+  expect(screen.getByRole('button')).toHaveTextContent('Enter');
 });
